bd62ef59286a78953720f07b8a5c9e706dd522a1
apanakkat <asheem@foreai.co>
2023-11-28 15:10:05 +0100

    New social card.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,8 +65,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/fore-ai-social-card.png',
       navbar: {
         title: '',
         logo: {
