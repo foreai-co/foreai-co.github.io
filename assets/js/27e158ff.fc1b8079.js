3763c5fd4e704ef57b300d920f74862a65ffb23b
Marcin Michalak <marcin@foreai.co>
2025-10-23 09:54:48 +0200

    Deploy website - based on 428bd4cd12cd473516263cc180b49dbfcfec59c6

--- assets/js/27e158ff.fc1b8079.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[111],{4014:e=>{e.exports=JSON.parse('{"title":"foresight","description":"foresight by fore ai.","slug":"/category/foresight","permalink":"/docs/category/foresight","navigation":{"next":{"title":"What is foresight?","permalink":"/docs/foresight/about"}}}')}}]);
